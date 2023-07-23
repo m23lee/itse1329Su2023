c5c4a1c8c96bbdba17e6f0b9fd318774d10e3e7d
m23lee <120135994+m23lee@users.noreply.github.com>
2023-07-22 23:44:06 -0500

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Matthew Lee, Advertising Conditional, 7/21/2023")
+    var friendsCount = 600
+    if (friendsCount >= 500) {
+        println("Adopt a Dog Today!")
+    } else {
+        println("Buy Cat Food At 20% Off")
+    }
+}
