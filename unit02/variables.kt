01469c01377b1f3e2ede89f092b5302dbec2a5b4
m23lee <120135994+m23lee@users.noreply.github.com>
2023-07-22 23:45:07 -0500

    Create variables.kt

---
+++ unit02/variables.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val myAge = 26
+    var averageAge = 26.00
+    averageAge = 28.00
+    val testNumber = 23
+    val evenOdd = testNumber%2
+    var answer = 0
+    answer = (answer + 1 + 10)*10
+
+    println(myAge)
+    println(averageAge)
+    println(testNumber)
+    println(evenOdd)
+    println(answer)
+
+}
