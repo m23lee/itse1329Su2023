5b9b8e8ab35960f75c03c22198bdd8f3a87f3fab
m23lee <120135994+m23lee@users.noreply.github.com>
2023-07-23 00:23:13 -0500

    Create magicNumbers.kt

---
+++ unit03/magicNumbers.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+println( "Please enter a month (in numeric form), a day, and a two-digit year.")
+
+    val month = 6
+    val day = 10
+    val year = 60
+
+if (year == month * day) {
+    println("This is a magic date!")
+ } else {
+     println("This is not a magic date :(")
+ }
+}
