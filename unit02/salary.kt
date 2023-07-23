1874ab0e4847a6c96c7c9bb128bcddff7420a901
m23lee <120135994+m23lee@users.noreply.github.com>
2023-07-22 23:44:41 -0500

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    println("Matthew Lee, Name, Age and Annual Income, 7/21/2023")
+
+    var name = "Average Joe"
+    var age = 38
+    var annualPay = 58000.00
+
+    println( "My name is $name , my age is $age and I hope to earn $$annualPay per year.")
+}
